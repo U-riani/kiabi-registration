e0dbea2407f837c4a68242a85fe31bce3d6a364b
U-riani <sandropapiashvili@gmail.com>
2026-01-06 15:53:46 +0400

    ss

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -671,6 +671,7 @@ const HomePage = () => {
             <button
               className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
               onClick={() => {
+                window.scrollTo({ top: 0, behavior: "instant" });
                 window.location.reload();
               }}
             >
